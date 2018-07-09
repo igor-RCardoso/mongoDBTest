9f3f10c2243294942dc953f52a58e0817b259eba
Your Name <igor.rodriguescardoso@gmail.com>
2018-07-09 14:02:14 -0300

    sera que vai

--- app/src/main/java/com/example/usuario/mongodbtest/MainActivity.java
+++ app/src/main/java/com/example/usuario/mongodbtest/MainActivity.java
@@ -5,17 +5,23 @@ import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
     ImageView logo;
+    EditText etUsr;
+    EditText etSenha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         logo = findViewById(R.id.logo_image_view);
         logo.setImageResource(R.drawable.logo);
+        etUsr = findViewById(R.id.et_login_usr);
+        etSenha = findViewById(R.id.et_login_senha);
+
 
 
         if (android.os.Build.VERSION.SDK_INT > 9)
@@ -33,5 +39,14 @@ public class MainActivity extends AppCompatActivity {
     public void cadastroMedico(View view){
         Intent it = new Intent(this, MedicoCadastroActivity.class);
         startActivity(it);
+    }
+
+    public void onClick(View view){
+        if(etSenha.getText().toString().equals("medico") && etUsr.getText().toString().equals("medico")){
+
+        }
+        else{
+
+        }
     }
 }
